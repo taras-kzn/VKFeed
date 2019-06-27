b7f3607f5d72d26654d7ce8deeb21bf659614d7d
denis <9172885049@mail.ru>
2019-06-27 12:19:49 +0300

    Скачал модель cleanSwift и вставил в проект

---
+++ VKNews/NewsFeed/NewsFeedModels.swift
@@ -0,0 +1,31 @@
+//
+//  NewsFeedModels.swift
+//  VKNews
+//
+//  Created by admin on 27/06/2019.
+//  Copyright (c) 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+enum NewsFeed {
+   
+  enum Model {
+    struct Request {
+      enum RequestType {
+        case some
+      }
+    }
+    struct Response {
+      enum ResponseType {
+        case some
+      }
+    }
+    struct ViewModel {
+      enum ViewModelData {
+        case some
+      }
+    }
+  }
+  
+}
---
+++ VKNews/NewsFeed/NewsFeedWorker.swift
@@ -0,0 +1,13 @@
+//
+//  NewsFeedWorker.swift
+//  VKNews
+//
+//  Created by admin on 27/06/2019.
+//  Copyright (c) 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedService {
+
+}
