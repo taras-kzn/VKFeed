e2e92ad0b9cf2391951fe0aeee0e5535fb8a31c6
denis <9172885049@mail.ru>
2019-07-02 20:57:45 +0300

    ver2

---
+++ VKNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNews
+//
+//  Created by admin on 26/06/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
